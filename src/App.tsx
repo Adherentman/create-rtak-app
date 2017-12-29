714a0a328342a8c043061aba2b62c671240f8881
XuZiHao <18357709818@163.com>
2017-12-29 17:06:00 +0800

    webpack

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default class App extends React.Component{
+  render(){
+    return <div>
+      Hello Webpack!!!!!
+      hello
+      <div>hello</div>
+      <div>hello</div>
+      <div>hi</div>
+    </div>
+  }
+}
