0d9fb0daec5e6c58960b3e53415db12fac5c43f2
XuZiHao <18357709818@163.com>
2017-12-29 17:29:32 +0800

    注释,生产环境加sourceMap

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+//开发环境
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,12 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
+//生产环境
 module.exports = merge(common, {
+  devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
   ]
 });
